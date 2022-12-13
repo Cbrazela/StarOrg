1336277efe219dffe061ed132a00532231b779c7
Cbrazela <cbraz005@fiu.edu>
2022-12-13 03:37:25 -0500

    update

---
+++ STARSORG/modDB.vb
@@ -0,0 +1,6 @@
+﻿Imports System.Data.SqlClient
+Module modDB
+    Public objSQLConn As SqlConnection
+    Public objSQLCommand As SqlCommand
+    Public gstrConn As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=C:\Users\cbraz\Documents\sample-8-Cbrazela\HenryDatabase\HBooks_2017.mdf;Integrated Security=True"
+End Module
---
+++ STARSORG/modErrHandler.vb
@@ -0,0 +1,66 @@
+﻿Module modErrHandler
+    'useful routines for input validation
+    Public Function ValidateTextBoxLength(ByRef obj As TextBox, ByRef errP As ErrorProvider) As Boolean
+        'This procedure validates a textbox is not empty
+        If obj.Text.Length = 0 Then
+            errP.SetIconAlignment(obj, ErrorIconAlignment.MiddleLeft)
+            errP.SetError(obj, "You must enter a value here")
+            obj.Focus()
+            Return False
+        Else
+            errP.SetError(obj, "")
+            Return True
+        End If
+    End Function
+    Public Function ValidateTextBoxNumeric(ByRef obj As TextBox, ByRef errP As ErrorProvider) As Boolean
+        'This procedure validate that a textbox has a numeric value
+        If Not IsNumeric(obj.Text) Then
+            errP.SetIconAlignment(obj, ErrorIconAlignment.MiddleLeft)
+            errP.SetError(obj, "You must enter a numeric value")
+            obj.Focus()
+            Return False
+        Else
+            errP.SetError(obj, "")
+            Return True
+        End If
+    End Function
+
+    Public Function ValidateTextBoxDate(ByRef obj As TextBox, ByRef errp As ErrorProvider) As Boolean
+        'This procedure validates that a textbox has a valid date value
+        If Not IsDate(obj.Text) Then
+            errp.SetIconAlignment(obj, ErrorIconAlignment.MiddleLeft)
+            errp.SetError(obj, "You must enter a valid date value")
+            obj.Focus()
+            Return False
+        Else
+            errp.SetError(obj, "")
+            Return True
+        End If
+    End Function
+
+    Public Function ValidateCombo(ByRef obj As ComboBox, ByRef errP As ErrorProvider) As Boolean
+        If obj.SelectedIndex = -1 Then
+            errP.SetIconAlignment(obj, ErrorIconAlignment.MiddleLeft)
+            errP.SetError(obj, "You must make a selection here")
+            obj.Focus()
+            Return False
+        Else
+            errP.SetError(obj, "")
+            Return True
+        End If
+    End Function
+
+    Public Function ValidateMaskedTextBoxLength(ByRef obj As MaskedTextBox, ByRef errP As ErrorProvider) As Boolean
+        If obj.Text.Length = 0 Then
+            errP.SetIconAlignment(obj, ErrorIconAlignment.MiddleLeft)
+            errP.SetError(obj, "You must enter a value here")
+            obj.Focus()
+            Return False
+        Else
+            errP.SetError(obj, "")
+            Return True
+        End If
+    End Function
+
+End Module
+
